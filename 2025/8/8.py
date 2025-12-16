703cc5b54c3097a860a52db07c4f44f9864d6ad4
kristiannilsson <77685746+kristiannilsson@users.noreply.github.com>
2025-12-16 22:44:01 +0100

    day 8

--- 2025/8/8.py
+++ 2025/8/8.py
@@ -1,21 +1,5 @@
+import heapq
 import math
-
-
-class CircuitNode:
-    def __init__(self, val):
-        self.val = val
-
-    def __lt__(self, other):
-        return self.val < other.val
-
-    def __repr__(self):
-        return str(self.val)
-
-    def distance(self, other):
-        x1, y1, z1 = self.val
-        x2, y2, z2 = other.val
-        return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
-
 
 with open("8.txt") as f:
     lines = f.read().splitlines()
@@ -24,36 +8,86 @@ coordinates = []
 
 for line in lines:
     raw_coordinates = map(int, line.split(","))
-    coordinates.append(CircuitNode(tuple(raw_coordinates)))
+    coordinates.append(tuple(raw_coordinates))
 coordinates.sort()
 
 
-def find_closest_node(node):
-    res = None
+def distance(coordinate, other_coordinate):
+    x1, y1, z1 = coordinate
+    x2, y2, z2 = other_coordinate
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
+
+
+def find_closest_coordinate(coordinate, coordinates):
     lowest_distance = float("inf")
-    for other_node in coordinates:
-        if other_node == node:
-            continue
-        distance = node.distance(other_node)
-        if distance < lowest_distance:
-            lowest_distance = distance
-            res = other_node
-        lowest_distance = min(lowest_distance, node.distance(other_node))
-    return res
+    closest_coordinate = None
+    for c in coordinates:
+        d = distance(coordinate, c)
+        if 0 < d < lowest_distance:
+            closest_coordinate = c
+            lowest_distance = d
+    return lowest_distance, closest_coordinate
 
 
-circuits = []
-for node in coordinates:
-    other_node = find_closest_node(node)
-    found_circuit = False
-    for circuit in circuits:
-        if other_node in circuit:
-            circuit.add(node)
-            found_circuit = True
-            break
-    if not found_circuit:
-        circuits.append(set([node, other_node]))
+distances = []
+for i in range(len(coordinates)):
+    for j in range(i + 1, len(coordinates)):
+        d = distance(coordinates[i], coordinates[j])
+        heapq.heappush(distances, (d, coordinates[i], coordinates[j]))
 
-circuits.sort(key=len, reverse=True)
-for circuit in circuits[:3]:
-    print(len(circuit))
+
+class UnionFind:
+    def __init__(self, elements):
+        self.parent = {element: element for element in elements}
+        self.rank = {element: 0 for element in elements}
+        self.size = {element: 1 for element in elements}
+
+    def find(self, element):
+        if self.parent[element] != element:  # Fixed
+            self.parent[element] = self.find(self.parent[element])  # Path compression
+        return self.parent[element]
+
+    def union(self, element1, element2):
+        root1 = self.find(element1)
+        root2 = self.find(element2)
+
+        if root1 == root2:
+            return False
+
+        if self.rank[root1] > self.rank[root2]:
+            self.parent[root2] = root1
+            self.size[root1] += self.size[root2]
+            self.size.pop(root2)
+        elif self.rank[root2] > self.rank[root1]:
+            self.parent[root1] = root2
+            self.size[root2] += self.size[root1]
+            self.size.pop(root1)
+        else:
+            self.rank[root1] += 1
+            self.size[root1] += self.size[root2]
+            self.parent[root2] = root1
+            self.size.pop(root2)
+        return True
+
+
+uf = UnionFind(coordinates)
+count = 0
+while distances:
+    if count == len(coordinates):
+        break
+    _d, coordinate, other_coordinate = heapq.heappop(distances)
+    uf.union(coordinate, other_coordinate)
+    count += 1
+
+circuit_sizes = sorted(uf.size.values(), reverse=True)
+result = circuit_sizes[0] * circuit_sizes[1] * circuit_sizes[2]
+print("part 1", result)
+
+
+last_coord1, last_coord2 = None, None
+while len(uf.size) > 1:
+    _d, coordinate, other_coordinate = heapq.heappop(distances)
+    if uf.union(coordinate, other_coordinate):
+        last_coord1, last_coord2 = coordinate, other_coordinate
+
+print("part 2:", last_coord1[0] * last_coord2[0])
